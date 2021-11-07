cf43afdc65f2e6faacea3c3c344ab3e0ed9071b8
Nikos Maniatis <49590528+maniatisni@users.noreply.github.com>
2021-11-07 19:40:41 +0200

    Added Comments and plot about cases in intensive care.

--- dashboard.py
+++ dashboard.py
@@ -8,52 +8,60 @@ import plotly.graph_objects as go
 pd.options.plotting.backend = "plotly"
 
 
-
-
+### Set Title Page, Header and Subheader
 st.set_page_config(page_title = "Greece Covid-19 Data Analysis")
 st.header('Greece Covid-19 Data Analysis')
 st.subheader("Nikos Maniatis")
 
+### Get "all" data from Coronavirus Greek API.
+### It contains info about:
+### 1. Confirmed Cases 2. Confirmed Deaths 3. Dates
 url = "https://covid-19-greece.herokuapp.com/all"
 response = requests.get(url)
 jsondata = response.json()
+
+### Read the JSON data into a pandas dataframe.
 columns=['confirmed','date','deaths']
 df = pd.DataFrame.from_dict(jsondata['cases'])
 data = []
 for x in jsondata['cases']:
     data.append([x['confirmed'],x['date'],x['deaths']])
-
 df = pd.DataFrame(data,columns=columns)
+
+### Calculate Daily cases/deaths from cumulative data.
 df['daily_cases'] = df['confirmed'] - df['confirmed'].shift(1)
 df['daily_deaths'] = df['deaths'] - df['deaths'].shift(1)
 df = df.dropna()
+### Convert daily data to integers
 df['daily_cases'] = pd.to_numeric(df['daily_cases'],downcast='integer')
 df['daily_deaths'] = pd.to_numeric(df['daily_deaths'],downcast='integer')
+### Convert Date column to datetime.
 df['date'] = pd.to_datetime(df['date'],format='%Y-%m-%d')
+
 ##################################
 #### DAILY CASES WITH M.A. PLOT###
 ##################################
 
+#make a copy of the dataframe and calculate moving average for 5 and 10 days.
 x = df.copy()
 x['SMA5'] = df['daily_cases'].rolling(window=5).mean()
 x['SMA10']= df['daily_cases'].rolling(window=10).mean()
 x = x.dropna(axis=0)
-fig = go.Figure()
 
+### Make the plot with PlotLy.
+fig = go.Figure()
 fig.add_trace(
     go.Line(
         x=x['date'],
         y=x['daily_cases'],
         name='Daily Cases'
     ))
-
 fig.add_trace(
     go.Line(
         x=x['date'],
         y=x['SMA5'],
         name='5 Day Moving Average'
     ))
-
 fig.add_trace(
     go.Line(
         x=x['date'],
@@ -64,12 +72,120 @@ fig['layout'].update(title='Daily Cases in Greece\n with Moving Averages',xaxis=
       tickangle=-30
     ))
 
+# Put the chart on the dashboard.
 st.plotly_chart(fig,use_container_width=True)
+
+############################
+#TESTS AND POSITIVITY RATE#
+############################
+
+### Get info about the total tests from the same API.
+url = "https://covid-19-greece.herokuapp.com/total-tests"
+response = requests.get(url)
+jsondata = response.json()
+### Convert the JSON data to pandas dataframe.
+columns=['date','rapid-tests','tests']
+tests = pd.DataFrame.from_dict(jsondata['total_tests'])
+data_ = []
+for k in jsondata['total_tests']:
+    data_.append([k['date'],k['rapid-tests'],k['tests']])
+tests = pd.DataFrame(data_,columns=columns)
+# Remove NaNs.
+tests['tests'] = tests['tests'].replace(np.nan,0).astype(int)
+tests['date'] = pd.to_datetime(tests['date'],format='%Y-%m-%d')
+
+# Choose cases after 26/2 because prior to that we didn't have data about tests.
+cases = df[df.date>= '2020-02-26']
+
+# MERGE the test data with the rest of our data on the 'date' column.
+comple = cases.merge(tests,how='left',on='date')
+# Calculate daily rapid tests and daily PCR tests
+comple['daily_rapid'] = comple['rapid-tests'] - comple['rapid-tests'].shift(1)
+comple['daily_tests'] = comple['tests'] - comple['tests'].shift(1)
+
+# Filter out the days with negative number of tests and drop NaNs.
+comple = comple[comple['daily_tests'] >= 0]
+comple = comple[comple['daily_rapid'] >= 0]
+comple.dropna(inplace=True)
+
+# Calculate Test Positivity Rate.
+# PR = 100*cases/total_tests
+comple['positivity'] = comple['daily_cases']/(comple['daily_rapid'] + comple['daily_tests'])
+# Replace NaNs and infinite positivity rates due to zero testing, with zero.
+comple['positivity'] = comple['positivity'].replace(np.nan,0)
+comple['positivity'] = comple['positivity'].replace(np.inf,0)
+comple['positivity'] = 100*comple['positivity']
+
+# Make the plot.
+fig5 = go.Figure()
+
+fig5.add_trace(
+    go.Scatter(
+        x=comple['date'],
+        y=comple['positivity'],
+        name='Positivity Rate'
+    ))
+fig5['layout'].update(title='Daily Test Positivity Rate (%)',xaxis=dict(
+      tickangle=-30
+    ))
+
+st.plotly_chart(fig5, use_container_width=True)
+
+fig6 = go.Figure()
+
+fig6.add_trace(
+    go.Scatter(
+        x=comple['date'],
+        y=comple['daily_rapid']+comple['daily_tests'],
+        name='Total Daily Tests (PCR + Rapid)'
+    ))
+fig6['layout'].update(title='Total Daily Tests (PCR + Rapid)',xaxis=dict(
+      tickangle=-30
+    ))
+
+st.plotly_chart(fig6, use_container_width=True)
+
+
+################################################
+### GET DATA ABOUT CASES IN INTENSIVE CARE ###
+################################################
+
+### Get Data from same API.
+url = "https://covid-19-greece.herokuapp.com/intensive-care"
+response = requests.get(url)
+jsondata = response.json()
+
+### Convert JSON data to pandas dataframe.
+columns=['date','intensive_care']
+meth = pd.DataFrame.from_dict(jsondata['cases'])
+data_ = []
+for k in jsondata['cases']:
+    data_.append([k['date'],k['intensive_care']])
+meth = pd.DataFrame(data_,columns=columns)
+
+
+### Make the Plot.
+
+fig7 = go.Figure()
+
+fig7.add_trace(
+    go.Scatter(
+        x=meth['date'],
+        y=meth['intensive_care'],
+        name='Number of Cases in Intensive Care'
+    ))
+fig7['layout'].update(title='Number of Cases in Intensive Care',xaxis=dict(
+      tickangle=-30
+    ))
+
+st.plotly_chart(fig7, use_container_width=True)
+
 
 ##################################
 #### DAILY DEATHS WITH M.A. PLOT###
 ##################################
 
+# Same as above, with the moving average, just with the deaths data.
 y = df.copy()
 y['SMA5'] = df['daily_deaths'].rolling(window=5).mean()
 y['SMA10']= df['daily_deaths'].rolling(window=10).mean()
@@ -101,10 +217,10 @@ fig2['layout'].update(title='Daily Deaths in Greece\n with Moving Averages',xaxi
     ))
 
 st.plotly_chart(fig2,use_container_width=True)
+
 ##################################
 #### TOTAL CONFIRMED CASES PLOT###
 ##################################
-
 
 fig3 = go.Figure()
 
@@ -138,66 +254,3 @@ fig4['layout'].update(title='Total Confirmed Deaths in Greece',xaxis=dict(
     ))
 
 st.plotly_chart(fig4, use_container_width=True)
-
-
-
-
-
-
-############################
-#TESTS AND POSITIVITY RATE#
-############################
-
-url = "https://covid-19-greece.herokuapp.com/total-tests"
-response = requests.get(url)
-jsondata = response.json()
-columns=['date','rapid-tests','tests']
-tests = pd.DataFrame.from_dict(jsondata['total_tests'])
-data_ = []
-for k in jsondata['total_tests']:
-    data_.append([k['date'],k['rapid-tests'],k['tests']])
-
-tests = pd.DataFrame(data_,columns=columns)
-tests['tests'] = tests['tests'].replace(np.nan,0).astype(int)
-tests['date'] = pd.to_datetime(tests['date'],format='%Y-%m-%d')
-
-cases = df[df.date>= '2020-02-26']
-comple = cases.merge(tests,how='left',on='date')
-comple['daily_rapid'] = comple['rapid-tests'] - comple['rapid-tests'].shift(1)
-comple['daily_tests'] = comple['tests'] - comple['tests'].shift(1)
-comple = comple[comple['daily_tests'] >= 0]
-comple = comple[comple['daily_rapid'] >= 0]
-comple.dropna(inplace=True)
-
-comple['positivity'] = comple['daily_cases']/(comple['daily_rapid'] + comple['daily_tests'])
-comple['positivity'] = comple['positivity'].replace(np.nan,0)
-comple['positivity'] = comple['positivity'].replace(np.inf,0)
-comple['positivity'] = 100*comple['positivity']
-
-fig5 = go.Figure()
-
-fig5.add_trace(
-    go.Scatter(
-        x=comple['date'],
-        y=comple['positivity'],
-        name='Positivity Rate'
-    ))
-fig5['layout'].update(title='Daily Test Positivity Rate (%)',xaxis=dict(
-      tickangle=-30
-    ))
-
-st.plotly_chart(fig5, use_container_width=True)
-
-fig6 = go.Figure()
-
-fig6.add_trace(
-    go.Scatter(
-        x=comple['date'],
-        y=comple['daily_rapid']+comple['daily_tests'],
-        name='Total Daily Tests (PCR + Rapid)'
-    ))
-fig6['layout'].update(title='Total Daily Tests (PCR + Rapid)',xaxis=dict(
-      tickangle=-30
-    ))
-
-st.plotly_chart(fig6, use_container_width=True)
